4e6c56b2a454c0e8a17ac6c96adbe7bc0297b0be
Eliezer Sant Ana <er.7santana@gmail.com>
2025-04-21 10:20:52 -0300

    troca automatica das imagens do carrossel

--- ChefDelivery/CarouselView/CarouselTabView.swift
+++ ChefDelivery/CarouselView/CarouselTabView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct CarouselTabView: View {
     
+    @State private var currentIndex = 0
+    
     let orders: [OrderType] = [
         OrderType(id: 0, name: "Pizza", image: "barbecue-banner"),
         OrderType(id: 1, name: "Sushi", image: "brazilian-meal-banner"),
@@ -16,13 +18,21 @@ struct CarouselTabView: View {
     ]
     
     var body: some View {
-        TabView {
+        TabView(selection: $currentIndex) {
             ForEach(orders) { order in
                 CarouselItemView(orderType: order)
+                    .tag(order.id)
             }
         }
         .tabViewStyle(PageTabViewStyle(indexDisplayMode: .always))
         .frame(height: 180)
+        .onAppear {
+            Timer.scheduledTimer(withTimeInterval: 5, repeats: true) { _ in
+                withAnimation {
+                    currentIndex = (currentIndex + 1) % orders.count
+                }
+            }
+        }
     }
 }
 
